aafa08cd863df8c7e866c5a634fa0c2bea2faf4f
KristianBulusan <kb546@njit.edu>
2023-11-13 22:28:58 -0500

    public_html

--- public_html/project/login.php
+++ public_html/project/login.php
@@ -4,11 +4,11 @@ require(__DIR__ . "/../../partials/nav.php");
 <form onsubmit="return validate(this)" method="POST">
     <div>
         <label for="email">Email/Username</label>
-        <input type="text" name="email" required />
+        <input type="text" placeholder="eg: johnDoe@mail.com" name="email" required />
     </div>
     <div>
         <label for="pw">Password</label>
-        <input type="password" id="pw" name="password" required minlength="8" />
+        <input type="password" id="pw" placeholder="8 characters" name="password" required minlength="8" />
     </div>
     <input type="submit" value="Login" />
 </form>
